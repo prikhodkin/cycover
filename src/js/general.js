d7d4fe88088e5412d52e45ed2946385b10ad9f6b
prikhodkin <info@prikhodkin.com>
2020-09-08 21:15:10 +0500

    Добавляет блок button

--- src/js/general.js
+++ src/js/general.js
@@ -1,11 +1,5 @@
 import Turbolinks from "turbolinks";
-import { Application } from "stimulus"
 import Tabs from "%modules%/tabs/tabs"
 import {$, $$} from "./util";
 Turbolinks.start();
 
-const application = Application.start();
-const tabs = $(`[data-tabs]`);
-
-
-new Tabs(tabs);
