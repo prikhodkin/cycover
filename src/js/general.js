1f9b9bf28fe4f5799e9dac9b63cd86a3a99c5e42
prikhodkin <info@prikhodkin.com>
2020-09-09 10:29:05 +0500

    Изменяет вызов функции для фикса ховера в меню

--- src/js/general.js
+++ src/js/general.js
@@ -2,14 +2,10 @@ import changeBurger from "%modules%/burger/burger";
 import {showMenu, editWidthMenu} from "%modules%/menu/menu";
 
 const items = document.querySelectorAll(`.menu__item`);
-
-if(window.matchMedia("(min-width: 1280px)").matches) {
-  editWidthMenu(items)
-}
-
 const itemsFooter = document.querySelectorAll(`.footer__item`);
 
 if(window.matchMedia("(min-width: 1280px)").matches) {
+  editWidthMenu(items)
   editWidthMenu(itemsFooter)
 }
 
