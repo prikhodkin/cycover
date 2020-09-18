c97bf4cceea63a8bef3cb9ab93bef8df17c5e7e4
SychugovKirill <9090000640@mail.ru>
2020-09-18 16:17:23 +0500

    Добавляет блок field-number

---
+++ src/blocks/field-numb/field-numb.js
@@ -0,0 +1,13 @@
+modules.define('field-numb', ['i-bem-dom'], function(provide, bemDom) {
+
+provide(bemDom.declBlock(this.name, {
+    onSetMod: {
+        js: {
+            inited: function() {
+                
+            }
+        }
+    }
+}));
+
+});
