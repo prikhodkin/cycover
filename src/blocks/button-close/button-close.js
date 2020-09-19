9f7ec4bf9253bb18d2132313acdf20e331dbc16c
SychugovKirill <9090000640@mail.ru>
2020-09-19 19:52:22 +0500

    Добавляет правки в калькулятор

---
+++ src/blocks/button-close/button-close.js
@@ -0,0 +1,13 @@
+modules.define('button-close', ['i-bem-dom'], function(provide, bemDom) {
+
+provide(bemDom.declBlock(this.name, {
+    onSetMod: {
+        js: {
+            inited: function() {
+                
+            }
+        }
+    }
+}));
+
+});
