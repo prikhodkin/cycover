9274df2fa2863217f096ee150ca0f43b942edb23
prikhodkin <info@prikhodkin.com>
2020-10-09 09:45:33 +0500

    Исправляет анимацию круга

--- src/js/general.js
+++ src/js/general.js
@@ -1,9 +1,6 @@
-import Turbolinks from "turbolinks";
 import { Application } from "stimulus"
 import { definitionsFromContext } from "stimulus/webpack-helpers"
 
-
-Turbolinks.start();
 
 const application = Application.start()
 const context = require.context(`./controllers`, true, /\.js$/);
