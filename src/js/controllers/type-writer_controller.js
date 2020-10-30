e654505c012fc289d1cd01bdd9206fc0322393cd
prikhodkin <info@prikhodkin.com>
2020-10-30 12:48:20 +0500

    Исправляет typewriter

--- src/js/controllers/type-writer_controller.js
+++ src/js/controllers/type-writer_controller.js
@@ -1,21 +1,20 @@
 import { Controller } from "stimulus";
 import Typewriter from 'typewriter-effect/dist/core';
+// https://www.npmjs.com/package/typewriter-effect Документация
 
 export default class extends Controller {
   static targets = [ `target` ]
+
   connect() {
     this.init();
   }
 
   init() {
-    console.log(this.targetTarget.getAttribute(`data-words`));
     const words = this.targetTarget.getAttribute(`data-words`).split(',');
-    console.log(words);
     new Typewriter(this.targetTarget, {
       strings: words,
       autoStart: true,
       loop: true
     });
   }
-
 }
